0677072e0e2f039e6bc90d31218428dad2d882ea
reddymh <reddymh@gmail.com>
2018-02-05 15:51:18 +0530

    Updated Rest URL

--- src/main/java/com/demo/docker/resource/HelloDocker.java
+++ src/main/java/com/demo/docker/resource/HelloDocker.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rest/hellodocker")
+@RequestMapping("/rest/microservicedemo")
 
 public class HelloDocker{
 
